53c6d8cfd71e9ee320d784ed5dcac9788af858fa
luci04 <avinashukla2000@gmail.com>
2022-05-22 03:48:15 +0530

    Expandable Card Framermotion Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transition={{ layout: { duration: 0.7, type: "spring" } }}
+        layout
+        className="card"
+        onClick={() => setIsOpen(!isOpen)}
+        //should be seperatey written
+        style={{
+          borderRadius: "1rem",
+          boxShadow: "0px 10px 30px rgba(0,0,0,0.5)",
+        }}
+      >
+        <motion.h2 layout="position">Framer Motion</motion.h2>
+
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="expand"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+              soluta! Praesentium corporis modi, mollitia voluptates maxime
+              similique at optio est.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+              soluta! Praesentium corporis modi.
+            </p>
+            <button>Read More</button>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
